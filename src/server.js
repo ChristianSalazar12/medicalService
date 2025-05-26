6c6c510114d7a94b778c42554334a1f74774a832
ChristianSalazar12 <cristianyamith@hotmail.com>
2025-05-26 13:02:51 -0500

    feat: add config server url

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3005;
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
